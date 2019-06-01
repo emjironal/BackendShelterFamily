f9610393fc6d3e2b3113c8f1917b81b4e9478df9
Adrián Garnier <garnier64@outlook.com>
2019-06-01 15:56:07 -0600

    Arreglo login

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var db = pgp(connectionUrl);
+var db = require('./basedatos').db;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
