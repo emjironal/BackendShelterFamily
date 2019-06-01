dc70a8bd1ad4611d7087c06fe08bab7836f5d906
GarnierAdrian <adrian.garnier2@gmail.com>
2019-06-01 17:30:24 -0600

    problemas load

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var refugiosRouter = require('./routes/refugios');
+var cargaRouter = require('./routes/carga')
+var administradorRouter = require('./routes/administrador')
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/refugios', refugiosRouter);
+app.use('/carga', cargaRouter);
+app.use('/administrador', administradorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/carga.js
+++ routes/carga.js
@@ -4,7 +4,38 @@ var db = require('./basedatos').db;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('carga', { title: 'Express' });
+});
+
+router.post('/load', function(req, res, next)
+{
+  var route = req.body.jsonFile;
+  $.getJSON(route, function(json){
+      console.log(json);
+  });
+  
+  var query = "select * from usuario where usuario = '" + username + "' and clave = '" + password +"'";
+  db.query(query)
+  .then(result =>
+    {
+      if(result.length < 1)
+      {
+        console.log('Result:', 'vacio', ' ', query);
+        res.redirect('/');
+      }
+      else
+      {
+        console.log('Result:', result[0]);
+        res.redirect('/carga');
+      }
+    }
+  )
+  .catch(err =>
+    {
+      console.log('Error: ', err);
+      res.redirect('/');
+    }
+  );
 });
 
 module.exports = router;
