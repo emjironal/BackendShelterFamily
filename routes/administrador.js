64f97e32e692d30540438584c4f64e812f3afb1d
GarnierAdrian <adrian.garnier2@gmail.com>
2019-06-01 20:09:16 -0600

    Ya están las estadisticas

--- routes/administrador.js
+++ routes/administrador.js
@@ -6,15 +6,38 @@ var select = require('./basedatos').select;
 /* GET home page. */
 router.get('/', function(req, res, next)
 {
-    db.query("Select r.idRefugio as id, f.nombre as nom, count(r.idRefugio) as total From Reserva r inner join Refugio f on (r.idRefugio = f.idrefugio) Group by 1,2 Order by 3 desc;")
-        .then(refugios=>{
-            res.render('administrador', {refugios: refugios});
+    db.query("Select r.cedula as cedula, c.nombre as nombre, count(r.cedula) From Reserva r inner join Cliente c on (r.cedula = c.cedula) Group by 1,2 Order by 3 desc;")
+        .then(personas=>{
+            db.query("Select a.idRefugio as ID , f.nombre as nombre, count(a.idRefugio) as Total From Animal a inner join Refugio f on (a.idRefugio = f.idrefugio) Group by 1,2 Order by 3 desc;")
+            .then(usuarios=>{
+                db.query("Select r.idRefugio as ID, f.nombre as Nombre, count(r.idRefugio) From Reserva r inner join Refugio f on (r.idRefugio = f.idrefugio) Group by 1,2 Order by 3 desc;")
+                .then(ref=>{
+                    db.query("Select distinct u.codigodistrito as codigo, d.nombre AS DISTRITO, p.nombre AS PROVINCIA, count(r.idrefugio) AS total From usuario u inner join refugio r on (u.idusuario = r.idrefugio) inner join distrito d on (d.codigo = u.codigodistrito) inner join provincia p on (d.idprovincia = p.idprovincia) Group by 1,2,3 Order by 2 desc;")
+                    .then(ubi=>{
+                        res.render('administrador', { personas: personas, users:usuarios , ref: ref, ubi: ubi});
+                    })
+                    .catch(err=>{
+                        console.log("Error: ", err);
+                        res.render('administrador', {personas: personas, users: usuarios, ref:ref});
+                    });
+                })
+                .catch(err=>{
+                    console.log("Error: ", err);
+                    res.render('administrador', {personas: personas, users: usuarios});
+                });
+                
+            })
+            .catch(err=>{
+                console.log("Error: ", err);
+                res.render('administrador', {personas: personas});
+            });
         })
         .catch(err=>{
             console.log("Error: ", err);
-            res.render('administrador', {refugios: []});
+            res.render('administrador', {personas: [], users:[], ref: [],ubi:[] });
         });
 });
+
 
 router.post("/modificar/:index/:id", function(req, res, next)
 {
